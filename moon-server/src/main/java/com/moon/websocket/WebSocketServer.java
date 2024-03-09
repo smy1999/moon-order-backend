2b12bf9a3416f0b7f6097adac6f9a70df4e14fcd
smy1999 <865988027@qq.com>
2024-03-09 10:20:42 +0800

    Update WebSocket Programming Style.

--- moon-server/src/main/java/com/moon/websocket/WebSocketServer.java
+++ moon-server/src/main/java/com/moon/websocket/WebSocketServer.java
@@ -21,7 +21,7 @@ import java.util.Map;
 public class WebSocketServer {
 
     //存放会话对象
-    private static Map<String, Session> sessionMap = new HashMap();
+    private static final Map<String, Session> sessionMap = new HashMap<>();
 
     /**
      * 连接建立成功调用的方法
